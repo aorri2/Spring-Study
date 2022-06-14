eb395cdd0582409884cc0e9d5234b813ac84116f
aorri2 <aorri96@naver.com>
2022-06-14 12:06:38 +0900

    Bean & XML Test

---
+++ hello-spring/core/src/test/java/howoocast/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package howoocast.core.xml;
+
+import howoocast.core.member.Member;
+import howoocast.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+    @Test
+    void xmlAPpContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
